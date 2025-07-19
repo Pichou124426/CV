efca530222beb06a20f7d8b15f7ebf94fd1df446
Madrid <pichou485@gmail.com>
2025-07-19 17:28:38 +0200

    back end okay

---
+++ backend/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+const pool = require("./db");
+
+const app = express();
+const PORT = 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/experiences", async (req, res) => {
+    try{
+        const result = await pool.query("SELECT * FROM experiences ORDER BY id");
+        res.json(result.rows);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server error");
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`🚀 Serveur lancé sur http://localhost:${PORT}`);
+});
+
