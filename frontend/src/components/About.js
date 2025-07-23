ca5e2d8ad50b8902d9d34fe186791cf1d379697c
MacNONO <pichou485@gmail.com>
2025-07-23 11:19:17 +0200

    maj About

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -23,7 +23,7 @@ function About() {
               <p className="about-text">
                 Bonjour, je m'appelle <strong>Noah</strong> 👋 <br />
                 Actuellement étudiant en <strong>Bachelor Informatique</strong> à l’École Hexagone de Clermont-Ferrand, je suis passionné par les projets concrets, le développement logiciel et l’innovation. <br /><br />
-                Grâce à mes expériences professionnelles, associatives et académiques, j’ai consolidé des compétences en <em>gestion de projet</em>, <em>développement front-end & IoT</em> et <em>communication d’équipe</em>. J’accorde une grande importance à la fiabilité, la rigueur, et l’élégance dans le code comme dans les relations humaines.
+                Grâce à mes expériences professionnelles, associatives et académiques, j’ai consolidé des compétences en <em>gestion de projet</em>, <em>développement fullstack & IoT</em> et <em>communication d’équipe</em>. J’accorde une grande importance à la fiabilité, la rigueur, et l’élégance dans le code comme dans les relations humaines.
               </p>
             </div>
           </Col>
