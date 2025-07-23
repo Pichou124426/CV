ac0efaf5c39f0497b007090cf5f1023c28effd99
MacNONO <pichou485@gmail.com>
2025-07-23 11:15:08 +0200

    modif db.js

--- backend/db.js
+++ backend/db.js
@@ -2,11 +2,10 @@ const { Pool } = require("pg");
 require("dotenv").config();
 
 const pool = new Pool({
-    host: process.env.PGHOST,
-    port: process.env.PGPORT,
-    database: process.env.PGDATABASE,
-    user: process.env.PGUSER,
-    password: process.env.PGPASSWORD,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // 🔒 requis pour Render en mode sécurité
+  },
 });
 
 module.exports = pool;
